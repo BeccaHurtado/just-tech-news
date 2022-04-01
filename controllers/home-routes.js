367f63eb062863dc3f033f6b389a25c1f7c0cb4d
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-04-01 15:44:29 -0700

    adding user authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -36,5 +37,15 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// login connection to handlebars
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
